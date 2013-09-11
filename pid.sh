76ac9a46a5a9bf172f7000255a2926ef14d6897b
Sébastien Luttringer <seblu@seblu.net>
2013-09-11 14:54:39 +0200

    pid.sh need count of pid to spawn
    default to 10 000 was too important

--- pid.sh
+++ pid.sh
@@ -1,7 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash
 # Copyright © 2013 Sébastien Luttringer
 
 declare -a pids
+declare -i count
+
+count="$1"
+
+if (( $count == 0 )); then
+	echo "usage: ${0##*/} number_of_pid" >&2
+	exit 1
+fi
 
 killall() {
 	[[ -n ${pids[*]} ]] && kill "${pids[@]}"
@@ -9,9 +17,11 @@ killall() {
 
 trap killall 0
 
-for i in $(seq 1 ${1:-10000}); do
+for i in $(seq 1 $c $count); do
 	sleep 365d &
 	pids+=("$!")
+	printf "%d " $!
 done
+echo
 echo 'Type Ctrl+C to kill all'
 wait
